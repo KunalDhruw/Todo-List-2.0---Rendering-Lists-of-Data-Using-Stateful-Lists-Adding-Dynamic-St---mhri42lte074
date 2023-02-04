43823429d8d47fb97e29eea3f732e54c0e9b09bb
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-02-04 23:00:07 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -2,12 +2,14 @@ import React from "react";
 import ListRender from "./ListRender";
 
 const Next7Days = (props) => {
-  const date = new Date("10/4/2022");
+  const date = new Date();
+  const endDate = new Date();
+  endDate.setDate(date.getDate() + 7);
   console.log(date);
   const nseven = props.list.filter((itm) => {
     const d = new Date(itm.date);
     console.log(d, "ddddd");
-    return d >= date;
+    return d >= date && d < endDate;
   });
   console.log(nseven);
   return (
