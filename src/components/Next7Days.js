7a2a07c08162f1dce51ef890585c605381c1ef0a
KunalDhruw <108332747+KunalDhruw@users.noreply.github.com>
2023-02-04 22:50:05 +0530

    Update Next7Days.js

--- src/components/Next7Days.js
+++ src/components/Next7Days.js
@@ -7,7 +7,7 @@ const Next7Days = (props) => {
   const nseven = props.list.filter((itm) => {
     const d = new Date(itm.date);
     console.log(d, "ddddd");
-    return d <= date;
+    return d >= date;
   });
   console.log(nseven);
   return (
